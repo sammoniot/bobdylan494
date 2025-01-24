972f328933e7e2f7194ed5928563b65ab1ed341b
Samantha M <sam7694@psu.edu>
2025-01-24 17:49:24 -0500

    pushed files to lyrics-txt

--- python/webscrape.py
+++ python/webscrape.py
@@ -1,10 +1,15 @@
 import string
 
+
 GENIUS_API_TOKEN='1eHSydZU6DqDROkteWbFhDQcZiuo8ncdHU4zmgTet2JAmnMJfl8ejnNI4uPSFwt5'
 from lyricsgenius import Genius
 from pathvalidate import sanitize_filename
 import os
 import re
+target_folder = r"C:\Users\sammo\OneDrive\Desktop\digit494\bobdylan494\lyrics-txt\earlydylan\thefreewheelin'bobdylan"
+
+if not os.path.exists(target_folder):
+    os.makedirs(target_folder)
 def cleaner_file_name(filename):
     invalid_chars = r"[,<>:\’\’\'/\|\?\*]"
     filename= re.sub(invalid_chars,"",filename)
@@ -14,13 +19,15 @@ genius = Genius(GENIUS_API_TOKEN)
 artist = genius.search_artist("Bob Dylan", max_songs=3, sort="title")
 print(artist.songs)
 
-album = genius.search_album("The Frewheelin' Bob Dylan", "Bob Dylan")
+album = genius.search_album("The Freewheelin' Bob Dylan", "Bob Dylan")
 
 for track in album.tracks:
     file_name = track.song.title + ".txt"
     cleaned_file_name = cleaner_file_name(sanitize_filename(file_name))
+    file_path = os.path.join(target_folder, cleaned_file_name)
+
     print("Downloading file: " +cleaned_file_name)
-    with open(cleaned_file_name, 'w', encoding="utf-8") as f:
+    with open(file_path, 'w', encoding="utf-8") as f:
         f.write(track.song.lyrics)
     #print(track.song.lyrics)
 
